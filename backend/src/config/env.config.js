d7b8b4d00605cf3b8ea6257ced1ff8d49087e75a
mostafa-ashour-dev <mostafa.ashour.dev@outlook.com>
2025-08-23 15:56:23 +0300

    (#1d) Backend Init: Installing the right eslint & prettier packages

--- backend/src/config/env.config.js
+++ backend/src/config/env.config.js
@@ -2,6 +2,5 @@ import { config } from "dotenv";
 config({ path: `.env.${process.env.NODE_ENV || "development"}.local` });
 
 const NODE_ENV = process.env.NODE_ENV;
-const PORT = process.env.PORT || 4755;
-
+const PORT = process.env.PORT || 47;
 export { NODE_ENV, PORT };
