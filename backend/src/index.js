008fb76115db0efc34fe66d7996559172aecd5c9
mostafa-ashour-dev <mostafa.ashour.dev@outlook.com>
2025-08-23 15:33:50 +0300

    (#1b) Backend init: Initialzed the express server

---
+++ backend/src/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import { NODE_ENV, PORT } from "./config/env.config.js";
+
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+
+// Routes
+
+
+// App initialization
+app.listen(PORT, () => {
+    console.log(`Server is running on: http://localhost:${PORT}\nEnvironment: ${NODE_ENV}\n`);
+});
